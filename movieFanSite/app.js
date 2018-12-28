60b7c4b10000fa3fcee49d25a62b2ccd2477c5f9
James Cherven <jcherven@mail.usf.edu>
2018-12-27 22:17:34 -0500

    initial setup of templates and frontend

--- movieFanSite/app.js
+++ movieFanSite/app.js
@@ -5,9 +5,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+const helmet = require('helmet');
 
 var app = express();
+app.use(helmet());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
