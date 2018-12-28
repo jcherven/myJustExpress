66e0f9d160926078010b8c98e370f6878bf22e39
James Cherven <jcherven@mail.usf.edu>
2018-12-27 20:18:40 -0500

    completes http video

--- myNewExpressSite/routes/index.js
+++ myNewExpressSite/routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    // const date = new Date(1969,6,20);
+    // res.set('Date', date);
+    // res.set('Cache-Control', 'no-store');
+    // res.set('Content-type', 'text/html');
+    // console.log(req.fresh);
+    // console.log(req.stale);
+    console.log(req.accepts('json'));
+    res.render('index', { title: 'Express' });
+
 });
 
 module.exports = router;
